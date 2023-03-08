fb944b38ab3056a18a767d04629bde0f5f9c0b87
Nicholas Tanugroho <117884155+Someonelse2309@users.noreply.github.com>
2023-03-08 18:25:07 +0700

    Add files via upload

--- 3_D_71220845.py
+++ 3_D_71220845.py
@@ -1,18 +1,20 @@
 import math
 loop = True
 while loop == True:
-    jarak = (input("Silahkan masukan jarak awal (dalam meter)\n>> ")).lower()
-    if jarak == "done" or jarak == "berhenti":
-        print ("\nTerima Kasih\n")
-        break
-    else:
-        jarak = int(jarak)
-        sudutlima = float(input("Silahkan masukan sudut elevasi di menit ke-5\n>> "))
-        sudutenam = float(input("Silahkan masukan sudut elevasi di menit ke-6\n>> "))
-        tansudutlima = math.tan(math.radians(sudutlima))
-        tansudutenam = math.tan(math.radians(sudutenam))
-        tinggilima = jarak * tansudutlima
-        jarakakhir = jarak * (tansudutenam - tansudutlima)
-        selisih = jarakakhir * tansudutenam
-        print (f"Ketinggian di menit 5 adalah {tinggilima}\nSelisih ketinggian di menit 5 dan 6 adalah {selisih}")
+    for i in range (0,1):
+        jarak = (input("\nSilahkan masukan jarak awal (dalam meter)\n>> ")).lower()
+        if jarak == "stop" or jarak == "berhenti":
+            print ("\nTerima Kasih\n")
+            loop = False
+            break
+        else:
+            jarak = int(jarak)
+            sudutlima = float(input("Silahkan masukan sudut elevasi di menit ke-5\n>> "))
+            sudutenam = float(input("Silahkan masukan sudut elevasi di menit ke-6\n>> "))
+            tansudutlima = math.tan(math.radians(sudutlima))
+            tansudutenam = math.tan(math.radians(sudutenam))
+            tinggilima = jarak * tansudutlima
+            jarakakhir = jarak * (tansudutenam - tansudutlima)
+            selisih = jarakakhir * tansudutenam
+            print (f"Ketinggian di menit 5 adalah {round(tinggilima,2)} meter\nSelisih ketinggian di menit 5 dan 6 adalah {round(selisih,2)} meter")
      
